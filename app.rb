176340b8f4d298c512d5d9624a11e457a573cc91
Stas <postyko.stas@gmail.com>
2025-03-02 00:15:44 +0000

    Added Contact entity and migration

--- app.rb
+++ app.rb
@@ -12,7 +12,43 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+class Contact < ActiveRecord::Base
+end
+
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@master = params[:master]
+	@color = params[:color]
+
+	Client.create :name => @username, :phone => @phone, :datestamp => @datetime, :barber => @master, :color => @color
+
+	erb '<h2>Дякую, вас було записано!</h2>'
+end
+
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	@email = params[:email]
+	@message = params[:message]
+	
+	Contact.create :email => @email, :message => @message
+
+	erb '<h2>Дякую, ваше повідомлення відправлено!</h2>'
 end
